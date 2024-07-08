6b860700527d0dd2080b26239c5f4420e7fd91ea
Laurent Pepin <laurentpepin79@gmail.com>
2024-07-07 20:05:22 -0500

    chore: [main][Laurent] React-Bootstrap implementation in main branch

--- stockontrol_frontend/src/App.js
+++ stockontrol_frontend/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Stockontrol</div>;
+  return (
+    <div className="App">
+      <h1>Stockontrol</h1>
+    </div>
+  );
 }
 
 export default App;
